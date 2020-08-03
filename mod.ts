7c33d0b6ba3f457a337af1c0825938c1bba93e78
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-08-03 10:31:15 +0200

    Filter
    Filter function on draws object.

--- mod.ts
+++ mod.ts
@@ -2,17 +2,36 @@ import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { join } from "http://deno.land/std/path/mod.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+interface Result {
+  [ key: string ]: string
+}
+
 async function loadResults() {
   const path = join("data", "wyniki.csv");
   const file = await Deno.open(path);
   const bufReader = new BufReader(file);
-  const result = await parse(bufReader, {
+  const draws = await parse(bufReader, {
     header: true,
     comma: ";"
   });
   Deno.close(file.rid); //Remember of that to awoid memory leak
 
-  return result;
+  return draws as Array<Result>;
 }
 
-const wyniki = await loadResults();
+const draws = await loadResults();
+
+function filterDraws(results: Result[], day1: number, day2: number, month: number, year: number){
+  const filteredDraws = results.filter((result: Result) => {
+    const dzien = +result["Dzien"];
+    const miesiac = +result["Miesiac"];
+    const rok = +result["Rok"];
+
+    return rok == year && miesiac == month && dzien >= day1 && dzien <= day2;
+  })
+  return filteredDraws;
+};
+
+const filteredDraws = filterDraws(draws, 31, 31, 7, 2020);
+
+console.log(filteredDraws, draws.length);
