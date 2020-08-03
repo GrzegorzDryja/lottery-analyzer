ac70c1891299be965ebf16995c45a540137980f3
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-08-03 08:52:22 +0200

    join
    Import join function from standard path to work on diferent operating systems

--- mod.ts
+++ mod.ts
@@ -1,8 +1,10 @@
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { join } from "http://deno.land/std/path/mod.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
 async function loadResults() {
-  const file = await Deno.open("./data/wyniki.csv");
+  const path = join("data", "wyniki.csv");
+  const file = await Deno.open(path);
   const bufReader = new BufReader(file);
   const result = await parse(bufReader, {
     header: true,
@@ -10,7 +12,7 @@ async function loadResults() {
   });
   Deno.close(file.rid); //Remember of that to awoid memory leak
 
-  console.log(result[22]);
+  return result;
 }
 
-await loadResults();
+const wyniki = await loadResults();
