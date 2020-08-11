042fe4be47a4daa8baac15f9dc257b0928fa42f6
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-08-11 15:15:00 +0200

    Table render

--- public/dist/main.js
+++ public/dist/main.js
@@ -181,18 +181,21 @@ System.register("components/draws", [], function (exports_3, context_3) {
                         .then((draws) => {
                         const hostElement = document.getElementById("host");
                         draws.forEach((draw) => {
-                            const drawsToColour = [+draw.L1, +draw.L2, +draw.L3, +draw.L4, +draw.L5, +draw.L6, +draw.L7, +draw.L8, +draw.L9, +draw.L10, +draw.L11, +draw.L12, +draw.L13, +draw.L14, +draw.L15, +draw.L16, +draw.L17, +draw.L18, +draw.L19, +draw.L20];
+                            const drawsToColour = [draw.L1, draw.L2, draw.L3, draw.L4, draw.L5, draw.L6, draw.L7, draw.L8, draw.L9, draw.L10, draw.L11, draw.L12, draw.L13, draw.L14, draw.L15, draw.L16, draw.L17, draw.L18, draw.L19, draw.L20];
                             console.log(drawsToColour);
-                            const headerTeat = `Losowanie nr: ${draw.Numer}, z dnia ${draw.Dzien}.${draw.Miesiac}.${draw.Rok}r.`;
+                            const headerText = `Losowanie nr: ${draw.Numer}, z dnia ${draw.Dzien}.${draw.Miesiac}.${draw.Rok}r.`;
                             const a = document.createElement("div");
                             a.className = "row";
                             const b = document.createElement("div");
                             b.className = "header";
-                            b.innerHTML = headerTeat;
+                            b.innerHTML = headerText;
                             a.appendChild(b);
                             for (let i = 1; i < 81; i++) {
                                 const c = document.createElement("div");
                                 c.className = "square";
+                                if (drawsToColour.includes(`${i}`) === true) {
+                                    c.classList.add("win");
+                                }
                                 a.appendChild(c);
                             }
                             hostElement.appendChild(a);
