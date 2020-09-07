3ae495d514f04b8878d80d92bfe9815362db9e4e
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-09-07 12:54:03 +0200

    Cleaning

--- public/src/app/app-main/components/draws/draws.component.ts
+++ public/src/app/app-main/components/draws/draws.component.ts
@@ -21,7 +21,28 @@ export class DrawsComponent implements OnInit {
       const hostElement = document.getElementById("host")!;
 
       draws.forEach((draw: Draw) => {
-        const drawsToColour = [draw.L1,draw.L2,draw.L3,draw.L4,draw.L5,draw.L6,draw.L7,draw.L8,draw.L9,draw.L10,draw.L11,draw.L12,draw.L13,draw.L14,draw.L15,draw.L16,draw.L17,draw.L18,draw.L19,draw.L20];
+        const drawsToColour = [
+          draw.L1,
+          draw.L2,
+          draw.L3,
+          draw.L4,
+          draw.L5,
+          draw.L6,
+          draw.L7,
+          draw.L8,
+          draw.L9,
+          draw.L10,
+          draw.L11,
+          draw.L12,
+          draw.L13,
+          draw.L14,
+          draw.L15,
+          draw.L16,
+          draw.L17,
+          draw.L18,
+          draw.L19,
+          draw.L20
+        ];
         const headerText = `Losowanie nr: ${draw.Numer}, z dnia ${draw.Dzien}.${draw.Miesiac}.${draw.Rok}r.`;          
 
         const a = document.createElement("div");
--- public/src/app/app-main/components/input/input.component.ts
+++ public/src/app/app-main/components/input/input.component.ts
@@ -17,6 +17,11 @@ export class AppMainInputComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  matchDiv(number: string){
+    let row = document.getElementById('main-row');
+    console.log(row);
+  }
+
   checkDraws(nr1: string, nr2: string, nr3: string, nr4: string, nr5: string){      
     return fetch(`http://localhost:8000/draws/${nr1},${nr2},${nr3},${nr4},${nr5}`)      
   }
