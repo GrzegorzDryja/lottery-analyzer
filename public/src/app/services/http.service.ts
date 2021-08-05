f4723690ef8e825d80e884de79bde77780559b8a
GrzegorzDryja <grzegorz.dryja@outlook.com>
2021-08-05 10:42:57 +0200

    app-main DI

--- public/src/app/services/http.service.ts
+++ public/src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Draw } from 'src/models/Draw.model'
 import { Lots } from 'src/models/Lots.model';
 
 @Injectable({
@@ -11,9 +12,13 @@ export class HttpService {
 
   constructor(private http: HttpClient) {}
 
-  checkDraw(lots: Lots): Observable<any> {
+  loadDraws(): Observable<Draw[]> {
+    return this.http.get<Draw[]>('http://localhost:8000/draws');
+  };
+
+  checkDraw(lots: Lots): Observable<Draw[]> {
     const lotsWithoutZeros: string = this.removeZeros(lots);
-    return this.http.get<any>(`${this.url}${lotsWithoutZeros}`);
+    return this.http.get<Draw[]>(`${this.url}${lotsWithoutZeros}`);
   }
 
   removeZeros(lots: Lots): string {
